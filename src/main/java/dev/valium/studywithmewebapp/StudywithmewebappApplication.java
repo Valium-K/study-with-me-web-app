32706f0a5017057b2280d64eaec57ef5d039472a
Valium-K <lschan96@gmail.com>
2021-08-06 17:54:00 +0900

    init

---
+++ src/main/java/dev/valium/studywithmewebapp/StudywithmewebappApplication.java
@@ -0,0 +1,13 @@
+package dev.valium.studywithmewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudywithmewebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudywithmewebappApplication.class, args);
+    }
+
+}
