a263341227e059ee6c0862df9419c56b64b008fc
Valium-K <lschan96@gmail.com>
2021-08-09 17:25:10 +0900

    회원가입 기능

---
+++ src/main/java/dev/valium/studywithmewebapp/controller/validator/SignUpFormValidator.java
@@ -0,0 +1,38 @@
+package dev.valium.studywithmewebapp.controller.validator;
+
+import dev.valium.studywithmewebapp.controller.dto.SignUpForm;
+import dev.valium.studywithmewebapp.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+// org.springframework.validation.Validator 임에 주의
+@Component
+@RequiredArgsConstructor
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        // Validation 대상 클래스스
+       return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        // email, nickname 중복여부 검사
+        SignUpForm signUpForm = (SignUpForm) target;
+
+        if(accountRepository.findByEmail(signUpForm.getEmail()) != null) {
+            errors.rejectValue("email", "invalid.email",
+                    new Object[]{signUpForm.getEmail()}, "이미 사용중인 이메일입니다.");
+        }
+
+        if(accountRepository.findByNickname(signUpForm.getNickname()) != null) {
+            errors.rejectValue("nickname", "invalid.nickname",
+                    new Object[]{signUpForm.getNickname()}, "이미 사용중인 닉네임입니다.");
+        }
+    }
+}
